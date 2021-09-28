9d225121661e52e5a99d7a69456aaeaaf7809ba6
jaehun-jeong <keepthefaith00@gmail.com>
2021-09-28 18:11:31 +0900

    list json 반환 예시

--- src/main/java/kr/ac/daegu/springbootapi/controller/BoardController.java
+++ src/main/java/kr/ac/daegu/springbootapi/controller/BoardController.java
@@ -1,21 +1,31 @@
 package kr.ac.daegu.springbootapi.controller;
 
-
+import kr.ac.daegu.springbootapi.model.TestDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @Slf4j
 public class BoardController {
 
     @RequestMapping(value = "/")
-    public String rootTest() throws Exception {
+    public List<TestDTO> rootTest() throws Exception {
         log.trace("logTest~~~~~~~~~~~~~~~~~~~~~~~~~~~~"); // 로그의 5단계 trace(일반적으로 남기는 이벤트 기록) -> debug(디버깅용) -> info(알림) -> warn(경고) -> error(에러)
         log.debug("logTest~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         log.info("logTest~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         log.warn("logTest~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         log.error("logTest~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-        return "hello!";
+
+        // 간단한 list 생성
+        List<TestDTO> testList = new ArrayList<TestDTO>();
+        testList.add(new TestDTO(1, "name1"));
+        testList.add(new TestDTO(2, "name2"));
+        testList.add(new TestDTO(3, "name3name3"));
+
+        return testList;
     }
 }
