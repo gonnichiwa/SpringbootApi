967b7d35208bdbbfdd96fc1d60f2885f45055a36
jaehun-jeong <keepthefaith00@gmail.com>
2021-09-28 20:49:49 +0900

    - BoardController, BoardService RENAME (TestController, TestService) - test패키지에 controller, model, service 집어넣음

--- src/main/java/kr/ac/daegu/springbootapi/test/controller/TestController.java
+++ src/main/java/kr/ac/daegu/springbootapi/test/controller/TestController.java
@@ -1,7 +1,7 @@
-package kr.ac.daegu.springbootapi.controller;
+package kr.ac.daegu.springbootapi.test.controller;
 
-import kr.ac.daegu.springbootapi.model.TestDTO;
-import kr.ac.daegu.springbootapi.service.BoardService;
+import kr.ac.daegu.springbootapi.test.model.TestDTO;
+import kr.ac.daegu.springbootapi.test.service.TestService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,12 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@RestController(value = "/test")
 @RequiredArgsConstructor
 @Slf4j
-public class BoardController {
+public class TestController {
 
-    public final BoardService boardService;
+    public final TestService testService;
 
     @GetMapping(value = "/")
     public List<TestDTO> rootTest() throws Exception {
@@ -25,8 +25,10 @@ public class BoardController {
         log.error("logTest~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 
         // 간단한 list 생성
-        List<TestDTO> testList = boardService.getTestList();
+        List<TestDTO> testList = testService.getTestList();
 
         return testList;
     }
+
+    /* mission */
 }
