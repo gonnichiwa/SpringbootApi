d85e861762cba98d1015fe451d3c5949f7cd5f77
jaehun-jeong <keepthefaith00@gmail.com>
2021-09-28 18:36:35 +0900

    RequestMapping -> GetMapping

--- src/main/java/kr/ac/daegu/springbootapi/controller/BoardController.java
+++ src/main/java/kr/ac/daegu/springbootapi/controller/BoardController.java
@@ -2,6 +2,9 @@ package kr.ac.daegu.springbootapi.controller;
 
 import kr.ac.daegu.springbootapi.model.TestDTO;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +15,7 @@ import java.util.List;
 @Slf4j
 public class BoardController {
 
-    @RequestMapping(value = "/")
+    @GetMapping(value = "/")
     public List<TestDTO> rootTest() throws Exception {
         log.trace("logTest~~~~~~~~~~~~~~~~~~~~~~~~~~~~"); // 로그의 5단계 trace(일반적으로 남기는 이벤트 기록) -> debug(디버깅용) -> info(알림) -> warn(경고) -> error(에러)
         log.debug("logTest~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
