2f81b86fb6aa7f0037bb3f074e97af8c5e28eb34
jaehun-jeong <keepthefaith00@gmail.com>
2021-09-28 19:05:34 +0900

    service layer 추가

--- src/main/java/kr/ac/daegu/springbootapi/controller/BoardController.java
+++ src/main/java/kr/ac/daegu/springbootapi/controller/BoardController.java
@@ -1,19 +1,20 @@
 package kr.ac.daegu.springbootapi.controller;
 
 import kr.ac.daegu.springbootapi.model.TestDTO;
+import kr.ac.daegu.springbootapi.service.BoardService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 @Slf4j
 public class BoardController {
+
+    public final BoardService boardService;
 
     @GetMapping(value = "/")
     public List<TestDTO> rootTest() throws Exception {
@@ -24,10 +25,7 @@ public class BoardController {
         log.error("logTest~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 
         // 간단한 list 생성
-        List<TestDTO> testList = new ArrayList<TestDTO>();
-        testList.add(new TestDTO(1, "name1"));
-        testList.add(new TestDTO(2, "name2"));
-        testList.add(new TestDTO(3, "name3name3"));
+        List<TestDTO> testList = boardService.getTestList();
 
         return testList;
     }
