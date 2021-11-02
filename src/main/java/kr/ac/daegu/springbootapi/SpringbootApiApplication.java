cdddc728e369b2bf5035f78877a452112d8bf90c
jaehun-jeong <keepthefaith00@gmail.com>
2021-11-02 21:00:02 +0900

    war 파일 빌드,패키징 하여 로컬에 설치된 tomcat의 webapp으로 돌 수 있도록

--- src/main/java/kr/ac/daegu/springbootapi/SpringbootApiApplication.java
+++ src/main/java/kr/ac/daegu/springbootapi/SpringbootApiApplication.java
@@ -5,6 +5,8 @@ import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
@@ -15,10 +17,15 @@ import javax.sql.DataSource;
 
 @SpringBootApplication
 @MapperScan(basePackages="kr.ac.daegu.springbootapi")
-public class SpringbootApiApplication {
+public class SpringbootApiApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootApiApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SpringbootApiApplication.class);
     }
 
     @Bean
